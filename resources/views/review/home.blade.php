4f8c3ba2077b2161cc53ce86f6bb49f4fd310de1
fujisan <gashigashi3040@gmail.com>
2020-04-12 00:41:42 +0900

    画像なし設定

--- resources/views/review/home.blade.php
+++ resources/views/review/home.blade.php
@@ -10,7 +10,11 @@
         <a class="col-lg-6 col-sm" href="{{ action('ReviewController@content', ['id' => $review->id]) }}" style="max-height: 400px;">
           <div class="row reviews">
             <div class="col-lg-6 col-sm-6">
+              @if (isset($review->book->image_path))
               <img src="{{ $review->book->image_path }}" alt="本の画像" width="250" height="350">
+              @else
+              <p>画像なし</p>
+              @endif
             </div>
             <div class="col-lg-6 col-sm-6">
               <h2>{{ $review->book->title }}</h2>
